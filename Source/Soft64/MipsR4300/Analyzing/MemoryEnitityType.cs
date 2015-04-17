6f0e5e58596753b5962851483add617553300d6e
bryanperris <bryanperris@live.com>
2015-04-17 01:16:29 -0400

    Started on a code analysis tool.

---
+++ Source/Soft64/MipsR4300/Analyzing/MemoryEnitityType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft64.MipsR4300.Analyzing
+{
+    public enum MemoryEnitityType
+    {
+        Fragment,
+        CodeBlock,
+        ResourceFile,
+        Bios
+    }
+}
