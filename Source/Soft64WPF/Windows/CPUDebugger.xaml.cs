23e5cc455424f565219f7f65adfff42ef95c3594
bryanperris <bryanperris@live.com>
2015-05-02 14:43:39 -0400

    [daily sync]

--- Source/Soft64WPF/Windows/CPUDebugger.xaml.cs
+++ Source/Soft64WPF/Windows/CPUDebugger.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Soft64;
 using Soft64.Debugging;
+using Soft64.MipsR4300.Debugging;
 using Soft64Binding.WPF;
 
 namespace Soft64WPF.Windows
@@ -34,11 +37,55 @@ namespace Soft64WPF.Windows
             }
 
             machineModel = (MachineViewModel)DataContext;
+
+            //WeakEventManager<Machine, LifeStateChangedArgs>.AddHandler(
+            //    Machine.Current,
+            //    "LifetimeStateChanged",
+            //    MachineStateChangedHandler
+            //    );
+        }
+
+        private void MachineStateChangedHandler(Object o, LifeStateChangedArgs args)
+        {
+            if (args.NewState == LifetimeState.Running)
+            {
+
+            }
+        }
+
+        private void ScanCode()
+        {
+            Stream vmemory = Machine.Current.CPU.VirtualMemoryStream;
+            Int64 begin = 0xA4000040;
+            Int64 current = vmemory.Position;
+            Stream view = new VMemViewStream();
+            view.Position = begin;
+            BinaryReader reader = new BinaryReader(view);
+
+            while (vmemory.Position < vmemory.Length)
+            {
+                try
+                {
+                    UInt64 read = reader.ReadUInt64();
+
+                    /* Parse instructions */
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            
+
+
         }
 
         private void xaml_BtnPause_Click(object sender, RoutedEventArgs e)
         {
             Debugger.Current.Pause();
+
+            ScanCode();
         }
 
         private void xaml_BtnResume_Click(object sender, RoutedEventArgs e)
