fdfa10b08f34798f357a76c3444083d6a991dcb7
bryanperris <bryanperris@live.com>
2015-05-31 16:08:32 -0400

    Try to simplify the hex editor control more, will need to profile it now that some logic is converted to C#

--- Source/Soft64.Toolkits.WPF/HexEditorRow.cs
+++ Source/Soft64.Toolkits.WPF/HexEditorRow.cs
@@ -4,27 +4,62 @@ using System.Text;
 using System.Windows;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace Soft64.Toolkits.WPF
 {
-    internal class HexEditorRow
+    internal class HexEditorRow : DependencyObject
     {
         private ObservableCollection<IndexedByte> m_RowBytes;
         private Int64 m_Address;
         private Int32 m_RowIndex;
+        private StackPanel m_HexRootPanel;
+        private StackPanel m_AsciiRootPanel;
+        private List<HexEditorTextBlock> m_HexBlocks;
+        private List<HexEditorTextBlock> m_AsciiBlocks;
 
         public HexEditorRow()
         {
             m_RowBytes = new ObservableCollection<IndexedByte>();
+            m_HexRootPanel = new StackPanel();
+            m_HexRootPanel.Orientation = Orientation.Horizontal;
+            m_AsciiRootPanel = new StackPanel();
+            m_AsciiRootPanel.Orientation = Orientation.Horizontal;
+            m_HexBlocks = new List<HexEditorTextBlock>();
+            m_AsciiBlocks = new List<HexEditorTextBlock>();
         }
 
         public void SetBytes(Byte[] bytes)
         {
             m_RowBytes.Clear();
+            m_HexRootPanel.Children.Clear();
+            m_AsciiRootPanel.Children.Clear();
+            m_HexBlocks.Clear();
+            m_AsciiBlocks.Clear();
 
             for(Int32 i = 0; i <bytes.Length; i++)
             {
                 m_RowBytes.Add(new IndexedByte() { ByteValue = bytes[i], Index = i });
+
+                HexEditorTextBlock hexBlock = new HexEditorTextBlock();
+                hexBlock.Index = i;
+                hexBlock.BlockType = BlockType.Hex;
+                hexBlock.RowIndex = RowIndex;
+                hexBlock.Text = bytes[i].ToString("X2");
+                hexBlock.Margin = new Thickness(2, 0, 0, 0);
+
+                HexEditorTextBlock asciiBlock = new HexEditorTextBlock();
+                asciiBlock.Index = i;
+                asciiBlock.BlockType = BlockType.Ascii;
+                asciiBlock.RowIndex = RowIndex;
+                asciiBlock.Text = new String((char)bytes[i], 1);
+                asciiBlock.Margin = new Thickness(2, 0, 0, 0);
+
+                m_HexBlocks.Add(hexBlock);
+                m_HexRootPanel.Children.Add(hexBlock);
+                m_AsciiRootPanel.Children.Add(asciiBlock);
+                m_AsciiBlocks.Add(asciiBlock);
             }
         }
 
@@ -50,11 +85,23 @@ namespace Soft64.Toolkits.WPF
             Bytes[byteOffset] = new IndexedByte() { Index = byteOffset, ByteValue = b };
             source.Position = Address + byteOffset;
             source.WriteByte(b);
+            m_AsciiBlocks[byteOffset].Text = new String((char)b, 1);
+            m_HexBlocks[byteOffset].Text = b.ToString("X2");
         }
 
         public Byte GetByteValue(Int32 offset)
         {
             return m_RowBytes[offset].ByteValue;
+        }
+
+        public Panel HexContent
+        {
+            get { return m_HexRootPanel; }
+        }
+
+        public Panel AsciiContent
+        {
+            get { return m_AsciiRootPanel; }
         }
     }
 }
