8dda244cca7f367d8f13353caf3f7222905d9119
Bryan Perris <bperris@checkernet.com>
2015-06-10 14:52:04 -0400

    Started work on CPU disassembly view control.

---
+++ Source/Soft64WPF/DisassemblyView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Soft64WPF
+{
+    public class DisassemblyView : Control
+    {
+        private StackPanel m_LineRootPanel;
+        private ObservableCollection<DiassemblyLine> m_LineCollection;
+    }
+
+    public sealed class DiassemblyLine
+    {
+
+    }
+}
