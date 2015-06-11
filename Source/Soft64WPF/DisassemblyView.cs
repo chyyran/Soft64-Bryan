b6b6c523ca6784ceb8e4ec76775d12e0c01eafaa
bryanperris <bryanperris@live.com>
2015-06-11 15:35:16 -0400

    More work on disassembly view WPF control.

--- Source/Soft64WPF/DisassemblyView.cs
+++ Source/Soft64WPF/DisassemblyView.cs
@@ -1,21 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using Soft64.MipsR4300;
+using Soft64.MipsR4300.Debugging;
+using ObservableDisasmLines = System.Collections.ObjectModel.ObservableCollection<Soft64WPF.DiassemblyViewLine>;
 
 namespace Soft64WPF
 {
-    public class DisassemblyView : Control
+    public partial class DisassemblyView : ContentControl
     {
-        private StackPanel m_LineRootPanel;
-        private ObservableCollection<DiassemblyLine> m_LineCollection;
-    }
+        private Stream m_VMemory;
+        private Int32 m_LineCount;
 
-    public sealed class DiassemblyLine
-    {
+        static DisassemblyView()
+        {
 
+        }
+
+        public DisassemblyView()
+        {
+            m_VMemory = new VMemViewStream();
+        }
+
+        private void ReadDiasm()
+        {
+            m_VMemory.Position = VMemoryOffset;
+            MipsInstruction[] instructions = new MipsInstruction[m_LineCount];
+            BinaryReader reader = new BinaryReader(m_VMemory);
+
+            for (Int32 i = 0; i < m_LineCount; i+=4)
+            {
+                instructions[i] = new MipsInstruction(0, reader.ReadUInt32());
+            }
+
+
+        }
+
+
+        private static readonly DependencyPropertyKey DisasmLinesPropertyKey =
+            DependencyProperty.RegisterReadOnly("DisasmLines", typeof(ObservableDisasmLines), typeof(DisassemblyView),
+            new PropertyMetadata(new ObservableDisasmLines()));
+
+        public static readonly DependencyProperty DisasmLinesProperty =
+            DisasmLinesPropertyKey.DependencyProperty;
+
+        public ObservableDisasmLines DisasmLines
+        {
+            get { return (ObservableDisasmLines)GetValue(DisasmLinesProperty); }
+            private set { SetValue(DisasmLinesPropertyKey, value); }
+        }
+
+        public static readonly DependencyProperty VMemoryOffsetProperty =
+            DependencyProperty.Register("VMemoryOffset", typeof(Int64), typeof(DisassemblyView),
+            new PropertyMetadata());
+
+        public Int64 VMemoryOffset
+        {
+            get { return (Int64)GetValue(VMemoryOffsetProperty); }
+            set { SetValue(VMemoryOffsetProperty, value); }
+        }
     }
 }
