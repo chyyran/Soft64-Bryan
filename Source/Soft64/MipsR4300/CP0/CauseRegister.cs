4ad5cb5012d5da32e07482b1863e8b6a053863e3
bryanperris <bryanperris@live.com>
2015-07-07 16:15:19 -0400

    Scrap some unused code, began working on exception handling and a nice way to do IO with complex registers.

---
+++ Source/Soft64/MipsR4300/CP0/CauseRegister.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft64.MipsR4300.CP0
+{
+    public sealed class CauseRegister
+    {
+        private UInt32 m_Reg;
+
+        public UInt32 RegValue
+        {
+            get { return m_Reg; }
+            set { m_Reg = value; }
+        }
+
+        public ExceptionCode ExcCode
+        {
+            get
+            {
+                Byte code = (Byte)((0xFFFFFF80 & m_Reg) >> 2);
+                return TranslateFromReg(code);
+            }
+
+            set
+            {
+
+            }
+        }
+
+
+        private ExceptionCode TranslateFromReg(byte value)
+        {
+            if ((value >= 0 && value <= 15) || value == 23 || value == 31)
+            {
+                return (ExceptionCode)value;
+            }
+            else
+            {
+                return ExceptionCode.Invalid;
+            }
+        }
+
+        private Byte TranslateFromCode(ExceptionCode code)
+        {
+            if (code == ExceptionCode.Invalid)
+                throw new ArgumentException("code");
+
+            return (Byte)code;
+        }
+    }
+
+    public enum ExceptionCode : byte
+    {
+        Interrupt = 0,
+        TlbMod,
+        TlbLoad,
+        TlbStore,
+        AddressErrorLoad,
+        AddressErrorStore,
+        BusErrorInstruction,
+        BusErrorData,
+        Syscall,
+        Breakpoint,
+        ReservedInstruction,
+        CopUnstable,
+        OverFlow,
+        Trap,
+        VCEInstruction,
+        FloatingPoint,
+        Watch = 23,
+        VCEData = 31,
+        Invalid
+    }
+}
---
+++ Source/Soft64/RegisterMap.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+using System.Collections;
+
+namespace Soft64
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class RegisterMappingAttribute : Attribute
+    {
+        private String m_FieldName;
+        private Int32 m_FieldSize;
+        private Int32 m_FieldOffset;
+        private RuntimeTypeHandle m_FieldType;
+
+        public RegisterMappingAttribute(String name, Int32 size, Int32 offset, Type type)
+        {
+            m_FieldName = name;
+            m_FieldSize = size;
+            m_FieldOffset = offset;
+            m_FieldType = type.TypeHandle;
+        }
+
+        public RuntimeTypeHandle FieldType
+        {
+            get { return m_FieldType; }
+        }
+
+        public Int32 FieldOffset
+        {
+            get { return m_FieldOffset; }
+        }
+
+        public Int32 FieldSize
+        {
+            get { return m_FieldSize; }
+        }
+
+        public String FieldName
+        {
+            get { return m_FieldName; }
+        }
+
+    }
+
+    public abstract class RegisterMap<T>
+        where T : struct
+    {
+        private T m_Register;
+        private IEnumerable<RegisterMappingAttribute> m_MapDefinitions;
+        private ExpandoObject m_DynamicObject;
+
+        protected RegisterMap()
+        {
+            m_MapDefinitions = GetType().GetCustomAttributes(typeof(RegisterMappingAttribute), true).OfType<RegisterMappingAttribute>();
+            m_DynamicObject = new ExpandoObject();
+        }
+
+        private void BuildProps()
+        {
+            Func<T> getter;
+            Action<T> setter;
+
+           /* 2 ^ x - 1 = bit mask
+            * So we need to figure out how many bits will be on the left side */
+
+           foreach (var def in m_MapDefinitions)
+           {
+               getter = () =>
+               {
+
+               };
+
+
+               ((IDictionary<String, Object>)m_DynamicObject).Add("get_" + def.FieldName, getter);
+           }
+        }
+
+        public T RegisterValue
+        {
+            get { return m_Register; }
+            set { m_Register = value; }
+        }
+    }
+}
