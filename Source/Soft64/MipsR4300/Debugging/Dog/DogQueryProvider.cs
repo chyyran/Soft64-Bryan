d77a86755fcb2d409ef928047d41271820164040
bryanperris <bryanperris@live.com>
2015-08-14 12:39:08 -0400

    Began work on dog classes

---
+++ Source/Soft64/MipsR4300/Debugging/Dog/DogQueryProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soft64.MipsR4300.Debugging.Dog
+{
+    internal sealed class DogQueryProvider : IQueryProvider
+    {
+        #region IQueryProvider Members
+
+        public IQueryable<TElement> CreateQuery<TElement>(System.Linq.Expressions.Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable CreateQuery(System.Linq.Expressions.Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(System.Linq.Expressions.Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Execute(System.Linq.Expressions.Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
